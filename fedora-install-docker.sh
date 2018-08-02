ad7bc48bb0ff16f952bb6a7a51a5859cdc0427f4
Rômulo Cabral Santos <romulosanttos1@gmail.com>
2018-08-02 10:26:51 -0300

    Update fedora-install-docker.sh
    adicionando clone do github e execução do docker-compose

--- fedora-install-docker.sh
+++ fedora-install-docker.sh
@@ -6,5 +6,7 @@ sudo dnf -y config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo && 
 
-sudo dnf -y install docker-ce && sudo dnf -y install docker-compose && sudo systemctl start docker && sudo docker run hello-world
+sudo dnf -y install docker-ce && sudo dnf -y install docker-compose && sudo systemctl start docker && sudo docker run hello-world &&
 
+git clone https://github.com/Romulosanttos/docker-compose-basic-database.git && cd docker-compose-basic-database && 
+sudo docker-compose up -d 
